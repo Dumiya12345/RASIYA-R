bf3aa3b1eb7494892cf6018f6331c171241b05d5
Dumiya12345 <dumiyadumiya070@gmail.com>
2025-02-10 05:46:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -37,10 +37,10 @@ function convertToBool(text, fault = 'true') {
 
 module.exports = {
   //  සෙසන් අයිඩී එක දාන්න👇👇👇 (උඩු කොමා දෙක ඇතුලේ)
-  SESSION_ID: process.env.SESSION_ID || "", 
+  SESSION_ID: process.env.SESSION_ID || "Golden-Queen-MD-p4ZBmRCI#rs2XWki0qhTjYvlDhAWT1IjT92DSxKm8WeDCwMUUkX0ftrc", 
 
 //  Mongodp URl එක දාන්න 👇👇👇 (උඩු කොමා දෙක ඇතුලේ )
-  MONGODB: process.env.MONGODB || "",
+  MONGODB: process.env.MONGODB || "mongodb+srv://yerix94679:<db_password>@cluster0.1umi9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
 
 
 MAX_SIZE: 500,
